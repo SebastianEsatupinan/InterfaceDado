62e745690ff56f436ae23a495b235a86ad0f674a
SebastianEsatupinan <sebastian.esatupinan@uao.edu.co>
2023-08-18 16:21:31 -0500

    Actualización del Controlador
    Se agrego todos lo llamados de los objetos que hay en la interface grafica junto con el nombramiento de cada acción respectiva.

--- src/interfacedado/FXMLDocumentController.java
+++ src/interfacedado/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interfacedado;
 
 import java.net.URL;
@@ -11,25 +6,58 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author sebastian.esatupinan
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label labelTitulo;
+
+    @FXML
+    private Label labelD1;
+
+    @FXML
+    private Label labelD2;
+    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField ValorD1;
+    
+    @FXML
+    private TextField ValorD2;
+     
+    @FXML
+    private TextArea AreaTextList;
+    
+    
+    @FXML
+    private void IniciarTiradas(ActionEvent event) {
+        
     }
+    
+    @FXML
+    private void PararTirada(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void TraerDatos(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void GuardarDatos(ActionEvent event) {
+        
+    }
+    
+    
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
