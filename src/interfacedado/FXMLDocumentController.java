02c359eb1dde0f1d7e28daa6917d6ae5dfca765f
DanielUAO <daniel_alberto.paz@uao.edu.co>
2023-08-20 23:08:14 -0500

    Actualización3
    Se organizó las funciones de los ActionEvent y se agregó correctamente la libreria

--- src/interfacedado/FXMLDocumentController.java
+++ src/interfacedado/FXMLDocumentController.java
@@ -1,5 +1,6 @@
 package interfacedado;
 
+import dato.Lanzamiento;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
@@ -11,6 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import modelo.Pila;
 
 public class FXMLDocumentController implements Initializable {
     
@@ -35,18 +37,48 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TextArea AreaTextList;
     
-    
+    Pila<Lanzamiento> pilaLanzamientos = new Pila<>();
+       
     @FXML
-    private void IniciarTiradas(ActionEvent event) {
+    private void IniciarTiradas(ActionEvent event) {   
+        
+     Random ran = new Random();
+        
+     IniciarTiradas.setDisable(true);
+     PararTirada.setDisable(false);
+
         
         
-        empezarTirada();
-  
+        TimerTask tarea = new TimerTask(){
+            @Override
+            public void run() {
+
+                int valorDado1 = ran.nextInt(6) + 1;
+                int valorDado2 = ran.nextInt(6) + 1;
+                
+                Lanzamiento objD = new Lanzamiento(valorDado1, valorDado2);
+                
+                pilaLanzamientos.apilar(objD);
+
+                ValorD1.setText(String.valueOf(valorDado1));
+                ValorD2.setText(String.valueOf(valorDado2));
+            }
+            };
+      
+        contador.scheduleAtFixedRate(tarea, 0, 4000);
+
     }
     
     @FXML
     private void PararTirada(ActionEvent event) {
-        detenerTirada();
+        // Habilitar el botón de iniciar y deshabilitar el de parar
+        IniciarTiradas.setDisable(false);
+        PararTirada.setDisable(true);
+
+        // Cancelar el temporizador
+        contador.cancel();
+        // Crear un nuevo temporizador para futuras tiradas
+        contador = new Timer();
     }
     
     @FXML
@@ -58,39 +90,7 @@ public class FXMLDocumentController implements Initializable {
     private void GuardarDatos(ActionEvent event) {
         
     }
-    
-    public void empezarTirada(){
-        
-        Random ran = new Random();
-        
-        IniciarTiradas(ActionEvent event).setEnabled(false);
-        PararTirada(ActionEvent event).setEnabled(true);
-
-        
-        
-        TimerTask tarea = new TimerTask(){
-            @Override
-            public void run() {
-                
-        ValorD1.setText(ran.nextInt(6) + 1);
-        ValorD2.setText(ran.nextInt(6) + 1);
-            }
-        };
       
-        contador.scheduleAtFixedRate(tarea, 0, 4000);
-
-    }
-    
-    public void detenerTirada(){
-        IniciarTiradas(ActionEvent event).setEnabled(true);
-        PararTirada(ActionEvent event).setEnabled(false);
-        contador.cancel();
-    }
-    
-    
-    
-    
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
